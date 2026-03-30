e15b2b0f34070df466ed5175c68a89bf8c5e3ae2
João <joaomesquita204@gmail.com>
2026-03-30 09:17:36 -0300

    feat: Declarações e variaveis

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,38 @@
-alert("Hello World ! ! !")
+// Declarações e variaveis
+// var, let e const
+var nome="Fiap";
+console.log(nome)
+
+var nome="Teste";
+console.log(nome)
+
+const taxaJuros=0.05
+console.log(taxaJuros)
+
+//typeof verifica o tipo da variavel
+
+let exemplo1="ola-dev-let"
+console.log(typeof exemplo1)
+
+var exemplo2="ola-dev-var"
+console.log(exemplo2)
+
+const exemplo3="ola-dev-const"
+console.log(exemplo3)
+
+let exemplo4=true
+console.log(exemplo4)
+
+let exemplo5={nome:"teste"};
+console.log(exemplo5)
+
+let exemplo6=["Java","C#","PHP"];
+console.log(exemplo6)
+
+// valor undefined
+let exemplo7
+console.log(exemplo7)
+
+//valor vazio
+let exemplo8
+console.log(exemplo8)
